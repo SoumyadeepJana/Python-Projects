4d584a841c1028eb734c5f5569ffd299578e80e2
Soumya_d_Coder <sjana646@gmail.com>
2017-07-01 00:15:27 +0530

    Update deals_of_the_day.py

--- deals_of_the_day.py
+++ deals_of_the_day.py
@@ -1,6 +1,7 @@
 #This program is solely meant for educational purposes.
 #This program fetches the deals of the day from flipkart.com and opens them in new tabs in your default browser.
 #The browser might crash sometimes as this is fully automated and a many tabs are opened at once.
+#This program is not futureProof(may not work in the future) if the HTML markup of the host site changes.
 
 #AUTHOR-Soumyadeep Jana
 
